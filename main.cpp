334c884b17a33952b35b4da99e1d685101639efe
v01d <150617039+candlemandle@users.noreply.github.com>
2024-05-22 13:26:53 +0300

    supposedly added the app's icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QMainWindow>
+#include <QIcon>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Set the application icon
+    a.setWindowIcon(QIcon(":/icons/appicon.ico"));
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
